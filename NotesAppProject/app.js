8e3c96098ff443b027c9154e8d8ebf43ca213aa6
MauroN <mauronav006@gmail.com>
2023-07-10 03:10:46 -0600

    Notes app basics

---
+++ NotesAppProject/app.js
@@ -0,0 +1,7 @@
+//const notes = require('./notes');
+const chalk = require('chalk');
+
+//notes('Your vote');
+
+console.log(chalk.bold.hex('#c733ff').inverse('This is crazy text'));
+console.log(chalk.green('Success!'));
---
+++ NotesAppProject/notes.js
@@ -0,0 +1,5 @@
+const getNotes = function (note) {
+    console.log(note);
+}
+
+module.exports = getNotes;
