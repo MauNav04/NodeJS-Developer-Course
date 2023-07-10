255c76cbd6f93282b307efe5b9438b95f58a8c36
MauroN <mauronav006@gmail.com>
2023-07-10 15:05:58 -0600

    Global variables

--- NotesAppProject/app.js
+++ NotesAppProject/app.js
@@ -3,5 +3,5 @@ const chalk = require('chalk');
 
 //notes('Your vote');
 
-console.log(chalk.bold.hex('#c733ff').inverse('This is crazy text'));
+console.log(chalk.bold.hex('#c7405f').inverse('This is a very crazy text'));
 console.log(chalk.green('Success!'));
