ab5aa871af5bc79c15b44d048b423800643fb611
MauroN <mauronav006@gmail.com>
2023-07-14 02:56:18 -0600

    Notes remove function v2

--- Playground/1-json.js
+++ Playground/1-json.js
@@ -13,18 +13,26 @@ const fs = require('fs');
 // fs.writeFileSync('1-json.json', obj2Str);
 
 
-const arr = ['a', 'b', 'c', 'd', 'e', 'f']
+const arr = [{ "title": "b" }, { "title": "a" }, { "title": "c" }, { "title": "d" }, { "title": "e" }, { "title": "f" }]
 
-console.log(arr.indexOf('d'))
-console.log(arr.indexOf('h'))
+arr.forEach(elem => {
+    if (elem.title === "b") {
+        console.log(true)
+    } else {
+        console.log(false)
+    }
+})
 
-let arrPiece1 = arr.slice(0, arr.indexOf('d'))
-let arrPiece2 = arr.slice(arr.indexOf('d'))
+// console.log(arr.indexOf('d'))
+// console.log(arr.indexOf('h'))
 
-arrPiece2.shift()
+// let arrPiece1 = arr.slice(0, arr.indexOf('d'))
+// let arrPiece2 = arr.slice(arr.indexOf('d'))
 
-const finalArr = arrPiece1.concat(arrPiece2)
-console.log(finalArr)
+// arrPiece2.shift()
+
+// const finalArr = arrPiece1.concat(arrPiece2)
+// console.log(finalArr)
 
 // console.log(arrPiece1)
 // console.log(arrPiece2)
