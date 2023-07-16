04f6e95bf0e12ff0510e4222257b8607bd0b5ddb
MauroN <mauronav006@gmail.com>
2023-07-15 23:20:47 -0600

    Fetching data form a weather API

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -1,7 +1,10 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(() => {
-    console.log('2 sec time out')
-}, 2000);
+const url = 'http://api.weatherstack.com/current?access_key=4b76536e77465809cc2c00c19bdf026a&query=37.8267,-122.4233'
 
-console.log('Stopping')
+request({ url: url, json: true }, (error, response) => {
+    const data = response.body.current
+    console.log(data.weather_descriptions[0] + '. It is currently ' + data.temperature + ' degrees out. It feels like '
+        + data.feelslike + ' degrees out.')
+
+})
